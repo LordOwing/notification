b3067838a536e3ded93b15c6b502863ef97f613e
LordOwing <michallolo12345@gmail.com>
2025-01-28 11:29:20 +0100

    28.01

--- app/src/main/java/com/example/notification/MainActivity.java
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
@@ -23,17 +25,22 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String CHANNEL_ID = "My_channel_id";
     private Button haha;
-
+    private Button dwa;
+    private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         createNotificationChannel();
-
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.banan);
         haha = findViewById(R.id.main_button);
         haha.setOnClickListener( v -> {
             sendNotification();
+        });
+        dwa = findViewById(R.id.second_button);
+        dwa.setOnClickListener( v -> {
+            sendNotificationLong();
         });
 
 
@@ -69,6 +76,33 @@ public class MainActivity extends AppCompatActivity {
                 .setSmallIcon(R.drawable.banan)
                 .setContentTitle("Nowe powiadomienie")
                 .setContentText("Tekst Powiadomeinia")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, builder.build());
+    }
+
+    void sendNotificationLong(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.banan)
+                .setContentTitle("Nowe powiadomienie")
+                .setContentText("Tekst Powiadomeinia")
+//                .setStyle(new NotificationCompat.BigTextStyle().bigText("Lorem Ipsum asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdsdasdasdasd"))
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
