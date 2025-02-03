a1d66306276ff352fc3fe002315f901bd45c121f
LordOwing <160587773+LordOwing@users.noreply.github.com>
2025-02-03 08:26:08 +0100

    Update NotificationHelper.java
    asdasd

--- app/src/main/java/com/example/notification/NotificationHelper.java
+++ app/src/main/java/com/example/notification/NotificationHelper.java
@@ -54,41 +54,6 @@ public class NotificationHelper  extends MainActivity {
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(Obiekt);
         notificationManager.notify(1, builder.build());
     }
-    static void sendNotification(String title, String message, String CHANNEL_ID, MainActivity Obiekt){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
-            CharSequence name = "Kanał Powiadomień";
-            String description = "Opis Kanału Powiadomień";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            NotificationManager notifiactionManager = Obiekt.getSystemService(NotificationManager.class);
-            notifiactionManager.createNotificationChannel(channel);
-        }
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
-            if(Obiekt.checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
-                    != PackageManager.PERMISSION_GRANTED){
-                Obiekt.requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
-                return;
-            }
-        }
-
-
-        Intent intent = new Intent(Obiekt, MainActivity.class);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(Obiekt, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(Obiekt, CHANNEL_ID)
-                .setSmallIcon(R.drawable.banan)
-                .setContentTitle("Nowe powiadomienie")
-                .setContentText("Tekst Powiadomeinia")
-                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
-
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(Obiekt);
-        notificationManager.notify(1, builder.build());
-    }
 
 }
